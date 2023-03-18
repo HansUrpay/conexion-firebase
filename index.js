1ea7aebe3568fa59297dda47c4abde4fc624a7c5
HansUrpay <hansurpayh@gmail.com>
2023-03-18 18:37:11 -0500

    fix: update index.js

--- index.js
+++ index.js
@@ -6,12 +6,6 @@ config();
 const port = process.env.PORT || 8000;
 
 
-// // app.post("/agregar", async (req, res) => {
-// //   const { body } = req;
-// //   await Contacto.add(body);
-// //   res.send({ msg: "Contact Added" });
-// // });
-
-app.listen(port, (req, res) => {
+app.listen(port, () => {
   console.log(`App en http://localhost:${port}`);
 });
